5fc0d42c40d0ca64f84b044a1146eade4e1a67fc
Your Name <kula.jega@gmail.com>
2020-07-28 15:59:39 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca0470fbc7ad863f5117a4bafca087d1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2cb23265-3974-4ec6-a594-a891aa632826'
+};
