09851d378fbeff11fd03bc252fff3c7743ce2603
Your Name <kula.jega@gmail.com>
2020-07-29 23:17:51 +0200

    store

--- api/book/controllers/book.js
+++ api/book/controllers/book.js
@@ -1,8 +1,52 @@
-'use strict';
+// 'use strict';
+
+// /**
+//  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+//  * to customize this controller
+//  */
+
+// module.exports = {};
+
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  async findByAuthor(ctx) {
+    let entities;
+
+    console.log("ctx", ctx);
+    return "Jega fais qqc";
+    // if (ctx.query._q) {
+    //   entities = await strapi.services.book.search(ctx.query);
+    // } else {
+    //   entities = await strapi.services.book.find(ctx.query);
+    // }
+
+    // return entities.map((entity) =>
+    //   sanitizeEntity(entity, { model: strapi.models.book })
+    // );
+  },
+};
+
+("use strict");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async findByAuthor(ctx) {
+    let entities;
+
+    entities = await strapi.services.book.findByAuthor(ctx.params.author);
+
+    return ctx.send("bonj");
+  },
+};
--- api/book/services/book.js
+++ api/book/services/book.js
@@ -1,8 +1,11 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
-
-module.exports = {};
+module.exports = {
+  findByAuthor(params) {
+    return params;
+  },
+};
